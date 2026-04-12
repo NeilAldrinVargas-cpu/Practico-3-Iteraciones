e45a2614374bb721002660a8bb44eb55d5ad1f95
NeilAldrinVargas-cpu <a2025211856@estudiantes.upsa.edu.bo>
2026-04-12 18:58:38 -0400

    Add files via upload

---
+++ Practico 3 ejercicio1 c++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    int suma=0;
+
+    cout<<"ingrse un numero entero positivo (N):";
+    cin>>N;
+
+    for (int i=1; i<=N; ++i){suma=suma + i; }
+
+    cout<<"=================================================================\n";
+    cout<<"La suma de los numeros enteros entre 1 y "<<N<<" es: "<<suma<<endl;
+    cout<<"=================================================================\n";
+
+    return 0;
+
+}
